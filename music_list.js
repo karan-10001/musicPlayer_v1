cbaf8a509589657059fa3116cccece1e296e50f8
Karan pratap Singh <85746435+karan-10001@users.noreply.github.com>
2022-08-19 16:42:25 +0530

    Add files via upload

---
+++ music_list.js
@@ -0,0 +1,44 @@
+let musicList = [
+    {
+        name: "Rise up",
+        artist: "TheFatRat",
+        img: "img/Rise_up.png",
+        src: "audio/TheFatRat-Rise_Up.mp3",
+        love: false
+    },
+    {
+        name: "Fairytale",
+        artist: "Alexander Rybak",
+        img: "img/fairytale.jpg",
+        src: "audio/Alexander_Rybak-Fairytale.mp3",
+        love: false
+    },
+    {
+        name: "Impossible",
+        artist: "James Arthur",
+        img: "img/impossible.jpg",
+        src: "audio/James_Arthur-Impossible.mp3",
+        love: false
+    },
+    {
+        name: "Landscape",
+        artist: "Jarico",
+        img: "img/landscape.jpg",
+        src: "audio/Jarico-Landscape.mp3",
+        love: false
+    },
+    {
+        name: "Diamonds",
+        artist: "Rihanna",
+        img: "img/diamonds.jpg",
+        src: "audio/Rihanna-diamonds.mp3",
+        love: false
+    },
+    {
+        name: "Hanuman ji bhajan",
+        artist: "karan",
+        img: "img/hanuman ji.jpg",
+        src: "audio/Hey Mahaveer.mp3",
+        love: false
+    }
+]
